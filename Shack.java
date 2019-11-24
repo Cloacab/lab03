59f3fd9548b9a1fa8afcf439a2f6bebcfc66a8a0
Cloacab <44143468+Cloacab@users.noreply.github.com>
2019-11-24 16:16:54 +0300

    first blueprint

---
+++ Shack.java
@@ -0,0 +1,24 @@
+private class Shack {
+     
+    static{
+        String name = "Typiachok";
+        String place = "Dryaning";
+        List<Room> rooms = new ArrayList<Room>();
+    }
+
+    void addRoom(Room room) {
+        this.rooms.add(room);
+    }
+
+    void removeRoom(Room room) {
+        this.rooms.remove(room);
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getPlace() {
+        return this.place;
+    }
+}
